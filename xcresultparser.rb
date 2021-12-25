3a44252bf077993c017638f0b43e953cea6ae1df
Alex da Franca <alex.dafranca@dpdhl.com>
2021-12-25 13:19:08 +0100

    updated xcresultparser to version 1.0.3

--- xcresultparser.rb
+++ xcresultparser.rb
@@ -4,7 +4,7 @@ class Xcresultparser < Formula
   # url "https://github.com/a7ex/xcresultparser.git",
   #    tag: "1.0.2", revision: "9977b9f765fca377755e378188ee8bc0e7e057d0"
   # version "1.0.2"
-  url "https://github.com/a7ex/xcresultparser/archive/1.0.2.tar.gz"
+  url "https://github.com/a7ex/xcresultparser/archive/1.0.3.tar.gz"
   sha256 "810184c3855ce9be400695aa1c28c98feff8aa015e101c197b41e8d76b1cf7a9"
   license "MIT"
 
