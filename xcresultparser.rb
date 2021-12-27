2f0900032d408144762a9d70cc81e7ccb25a098d
Alex da Franca <alex.dafranca@dpdhl.com>
2021-12-27 13:44:23 +0100

    updated xcresultparser to version 1.0.4

--- xcresultparser.rb
+++ xcresultparser.rb
@@ -3,9 +3,9 @@ class Xcresultparser < Formula
   homepage "https://github.com/a7ex/xcresultparser"
   # url "https://github.com/a7ex/xcresultparser.git",
   #    tag: "1.0.2", revision: "9977b9f765fca377755e378188ee8bc0e7e057d0"
-  # version "1.0.2"
-  url "https://github.com/a7ex/xcresultparser/archive/1.0.3.tar.gz"
-  sha256 "810184c3855ce9be400695aa1c28c98feff8aa015e101c197b41e8d76b1cf7a9"
+  version "1.0.4"
+  url "https://github.com/a7ex/xcresultparser/archive/1.0.4.tar.gz"
+  sha256 "3d732552a7af37c17966f8f6b2e95fd7d07efa126f3e14745ce2ed426f063573"
   license "MIT"
 
   depends_on xcode: ["10.0", :build]
